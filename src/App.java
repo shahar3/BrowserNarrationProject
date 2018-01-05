460877ceb59b790ed54b07426dc3848328f55913
shahar3 <shahar061@gmail.com>
2018-01-05 13:52:38 +0200

    updated

--- src/App.java
+++ src/App.java
@@ -11,12 +11,17 @@ public class App {
     private static final String URL_LINK = "http://edition.cnn.com/US/OJ/"; //the webpage url
 
     public static void main(String[] args){
+        System.out.println("Connecting to "+ URL_LINK);
         //1. connect to the web
         WebUtil.connectToWebsite(URL_LINK);
         //2. extract the tags from the web
         ArrayList<String> tags = WebUtil.getItems();
-
+        
         //test - print all the tags
+        //testTags(tags);
+    }
+
+    private static void testTags(ArrayList<String> tags) {
         for (String tag:tags){
             System.out.println(tag);
         }
