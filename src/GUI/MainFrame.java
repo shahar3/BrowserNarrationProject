b797ed7deb737e4c0a71c5a6ac2c6f6656303be5
shahar3 <shahar061@gmail.com>
2018-01-05 15:35:10 +0200

    updated

---
+++ src/GUI/MainFrame.java
@@ -0,0 +1,17 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+ * Created by shaha on 05/01/2018.
+ */
+public class MainFrame extends JFrame {
+    private final String TITLE = "3D Narration for web browser";
+
+    public MainFrame(){
+        setTitle(TITLE);
+        setSize(800,600);
+        setVisible(true);
+    }
+}
