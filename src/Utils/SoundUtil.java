d31fd4832626093b8de384d8139c564cd42add9f
shahar3 <shahar061@gmail.com>
2018-01-06 17:36:24 +0200

    fixed the filenames

--- src/Utils/SoundUtil.java
+++ src/Utils/SoundUtil.java
@@ -1,11 +1,22 @@
 package Utils;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class SoundUtil {
     private static final ArrayList<String> voices = new ArrayList<>();
 
     public static void createWavFiles(ArrayList<String> tags) {
+        //clear audio directory
+        try {
+            FileUtils.deleteDirectory(new File("audio"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         //init the voices
         initVoices();
 
@@ -13,10 +24,10 @@ public class SoundUtil {
         for (int i = 0; i < tags.size(); i++) {
             String fileName = tags.get(i);
             //replace the '.' with blank space
-            fileName = fileName.replaceAll("[^A-Za-z0-9]"," ");
+            fileName = fileName.replaceAll("[^A-Za-z0-9 ]","");
 
             tts.setVoice(voices.get(i%5));
-            tts.saveToFile(tags.get(i), 1.0f, false, false, tags.get(i));
+            tts.saveToFile(tags.get(i), 1.0f, false, false, fileName);
         }
     }
 
