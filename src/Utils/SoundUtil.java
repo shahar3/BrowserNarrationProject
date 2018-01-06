2f487602885f1c8c797802e1e6a989a5736799f5
shahar3 <shahar061@gmail.com>
2018-01-06 17:18:40 +0200

    fixed the filenames

--- src/Utils/SoundUtil.java
+++ src/Utils/SoundUtil.java
@@ -11,6 +11,10 @@ public class SoundUtil {
 
         TextToSpeech tts = new TextToSpeech();
         for (int i = 0; i < tags.size(); i++) {
+            String fileName = tags.get(i);
+            //replace the '.' with blank space
+            fileName = fileName.replaceAll("[^A-Za-z0-9]"," ");
+
             tts.setVoice(voices.get(i%5));
             tts.saveToFile(tags.get(i), 1.0f, false, false, tags.get(i));
         }
